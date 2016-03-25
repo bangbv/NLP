d8d15644c06f4e841e3ec7ce0c9523b231f19506
bangbv <vanbang0208@gmail.com>
2016-03-25 10:53:20 +0700

    update

--- ex3.pl
+++ ex3.pl
@@ -1,1 +1,5 @@
-hello prolog
+happy(vincent).
+listen2Music(butch).
+playsAirGuitar(vincent):-listen2Music(vincent),happy(vincent).
+playsAirGuitar(butch):-happy(butch).
+playsAirGuitar(butch):-listen2Music(butch).
